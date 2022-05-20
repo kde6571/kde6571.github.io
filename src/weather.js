d4d830c0b594d4fa2827b081ab5ff7994874157f
kde6571 <105931791+kde6571@users.noreply.github.com>
2022-05-21 08:22:58 +0900

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,6 @@
 const weatherContainer = document.getElementById("weather");
 const weather = [];
+let displayWeather = "DETECTING...";
 		
 function onGetGeoSuccess(posision)
 {
@@ -8,18 +9,11 @@ function onGetGeoSuccess(posision)
 	const API_KEY = "c72fa06e3b4d98f20cdf76a3922451a6";
 	const apiStr = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 	fetch(apiStr).then(response => response.json()).then(data => {
+		displayWeather = `Location : ${data.name} || Weather : ${data.weather[0].main} || Temperture : ${data.main.temp}`;
 		
-		weather.push("Location : " + data.name);
-		weather.push(" || Weather : " + data.weather[0].main);
-		weather.push(" || Temperture : " + data.main.temp);	
-		
-		for(let T = 0 ; T < 3 ; T ++)
-		{
-			const span = document.createElement("span");
-			weatherContainer.appendChild(span);
-			
-			span.innerText = weather[T];
-		}
+		const p = document.createElement("p");
+		p.innerText = displayWeather;
+		weatherContainer.appendChild(p);
 	});
 }
 
